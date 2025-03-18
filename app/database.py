90ffd98f113808e37d47c4d2df0b3700722e5bff
Tornesko <kostiantyn.borovenskyi@raccoongang.com>
2025-03-18 20:43:13 +0200

    dockerize

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 database = Database(DATABASE_URL)
 metadata = MetaData()
 engine = create_engine(DATABASE_URL)
